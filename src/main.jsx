9d754cbee3e00c77b446e69ab4cc25eb6f94b40b
NouranAlSharawneh <noransharawna@hotmail.com>
2025-01-13 10:21:52 +0300

    updating the file structure + adding react-redux package

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
+import store from "./store.jsx";
+
+store.dispatch({ type: "account/deposit", payload: 500 });
+console.log(store.getState());
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
